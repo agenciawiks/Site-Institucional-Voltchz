be4e7d579b46d415cef605e48eda5b9ce5db3a00
agenciawiks <agenciawiks@gmail.com>
2026-05-22 14:23:24 -0300

    design: adicionar mais imagens de clientes e usar .webp

--- js/config.js
+++ js/config.js
@@ -3,36 +3,45 @@
  */
 
 export const CONFIG = {
-  ASSETS: {
-    CLIENTS: [
-      'IMAGENS/CLIENTES/cliente-3.avif',
-      'IMAGENS/CLIENTES/cliente-4.avif',
-      'IMAGENS/CLIENTES/cliente-5.avif',
-      'IMAGENS/CLIENTES/cliente-6.avif',
-      'IMAGENS/CLIENTES/cliente-7.avif',
-      'IMAGENS/CLIENTES/cliente-8.avif',
-      'IMAGENS/CLIENTES/cliente-9.avif',
-      'IMAGENS/CLIENTES/cliente-10.avif',
-      'IMAGENS/CLIENTES/cliente-11.avif',
-      'IMAGENS/CLIENTES/cliente-12.avif',
-      'IMAGENS/CLIENTES/cliente-13.avif',
-      'IMAGENS/CLIENTES/cliente-14.avif',
-      'IMAGENS/CLIENTES/cliente-15.avif',
-    ],
-    LOGOS: [
-      'IMAGENS/LOGOS-CLIENTES/logo-1.svg',
-      'IMAGENS/LOGOS-CLIENTES/logo-2.png',
-      'IMAGENS/LOGOS-CLIENTES/logo-3.png',
-    ],
-    DEFAULT_LOGO: 'IMAGENS/logo.webp'
-  },
-  ANIMATIONS: {
-    AUTOPLAY_HERO: 6000,
-    AUTOPLAY_CLIENTS: 5000,
-    COUNTER_DURATION: 1800
-  },
-  CONTACT: {
-    WHATSAPP: '5512981039845',
-    EMAIL: 'contato@voltchz.com.br'
-  }
+    ASSETS: {
+        CLIENTS: [
+            'IMAGENS/CLIENTES/cliente-1.webp',
+            'IMAGENS/CLIENTES/cliente-2.webp',
+            'IMAGENS/CLIENTES/cliente-3.webp',
+            'IMAGENS/CLIENTES/cliente-4.webp',
+            'IMAGENS/CLIENTES/cliente-5.webp',
+            'IMAGENS/CLIENTES/cliente-6.webp',
+            'IMAGENS/CLIENTES/cliente-7.webp',
+            'IMAGENS/CLIENTES/cliente-8.webp',
+            'IMAGENS/CLIENTES/cliente-9.webp',
+            'IMAGENS/CLIENTES/cliente-10.webp',
+            'IMAGENS/CLIENTES/cliente-11.webp',
+            'IMAGENS/CLIENTES/cliente-12.webp',
+            'IMAGENS/CLIENTES/cliente-13.webp',
+            'IMAGENS/CLIENTES/cliente-14.webp',
+            'IMAGENS/CLIENTES/cliente-15.webp',
+            'IMAGENS/CLIENTES/cliente-16.webp',
+            'IMAGENS/CLIENTES/cliente-17.webp',
+            'IMAGENS/CLIENTES/cliente-18.webp',
+            'IMAGENS/CLIENTES/cliente-19.webp',
+            'IMAGENS/CLIENTES/cliente-20.webp',
+            'IMAGENS/CLIENTES/cliente-21.webp',
+            'IMAGENS/CLIENTES/cliente-22.webp',
+        ],
+        LOGOS: [
+            'IMAGENS/LOGOS-CLIENTES/logo-1.svg',
+            'IMAGENS/LOGOS-CLIENTES/logo-2.png',
+            'IMAGENS/LOGOS-CLIENTES/logo-3.png',
+        ],
+        DEFAULT_LOGO: 'IMAGENS/logo.webp'
+    },
+    ANIMATIONS: {
+        AUTOPLAY_HERO: 6000,
+        AUTOPLAY_CLIENTS: 5000,
+        COUNTER_DURATION: 1800
+    },
+    CONTACT: {
+        WHATSAPP: '5512981039845',
+        EMAIL: 'contato@voltchz.com.br'
+    }
 };
