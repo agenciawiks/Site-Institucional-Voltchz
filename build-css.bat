d833909a3838a40771eeaae0c2639493c720106c
agenciawiks <agenciawiks@gmail.com>
2026-07-16 15:43:42 -0300

    build: remove test_runner, batch script e binario do tailwind da branch de producao

--- build-css.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Compila o CSS do Tailwind localmente (sem depender do CDN em producao).
-REM Rode este script sempre que adicionar/alterar classes Tailwind no projeto.
-tools\tailwindcss.exe -i tailwind-src.css -o static\tailwind.min.css --minify
